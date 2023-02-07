bcc069e03a009cde8cec873ab46640c1782390e6
Tiago Santos <tiagopatriciosantos@gmail.com>
2023-02-07 17:59:40 +0000

    Deploy

--- builddeploy.bat
+++ builddeploy.bat
@@ -1,7 +1,7 @@
 call .\.venv\Scripts\activate
 :: if prerelease run this instead of path-> call poetry version prerelease
 :: call poetry version patch
-call poetry version prerelease
+call poetry version patch
 call poetry build
 call poetry publish -u __token__ --password %POETRY_PYPI_TOKEN%
 
